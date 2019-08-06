318998d01da4c82b970b6948cc677553cb755543
Kainã Almeida <kainafa@hotmail.com>
2019-08-06 10:24:55 -0300

    KA - Detalhe Oferta

--- lib/pages/detalhe-oferta.page.dart
+++ lib/pages/detalhe-oferta.page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DetalheOfertaPage extends StatefulWidget {
@@ -30,6 +31,84 @@ class _DetalheOfertaPageState extends State<DetalheOfertaPage> {
               child: GoogleMap(
                 mapType: MapType.hybrid,
                 initialCameraPosition: _kGooglePlex,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 15,
+                right: 15,
+              ),
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.pin_drop,
+                            size: 30,
+                          ),
+                          Text(
+                            "Arroio do Meio/RS",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Mercadoria: PERECIVEIS",
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("Quantidade: 198,00"),
+                          Text("Peso: 157,00"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Icon(
+                            Icons.thumb_up,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {},
+                        ),
+                        FlatButton(
+                          child: Icon(
+                            FontAwesomeIcons.whatsapp,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {},
+                        ),
+                        FlatButton(
+                          child: Icon(
+                            Icons.phone,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
